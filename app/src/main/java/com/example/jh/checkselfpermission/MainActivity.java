dfc5f30383ce8029947813a9863c638bd2cb00e7
JinHui <hui.jin@boatchina.net>
2017-12-05 17:14:11 +0800

    权限不提示该有的弹窗，真是纳闷了！

--- app/src/main/java/com/example/jh/checkselfpermission/MainActivity.java
+++ app/src/main/java/com/example/jh/checkselfpermission/MainActivity.java
@@ -1,16 +1,132 @@
 package com.example.jh.checkselfpermission;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Android 开发测试权限！项目中需要。
+ * android6.0运行时权限扩展篇
+ * 什么情况下需要动态获取权限：
+ * 满足两个条件：①6.0以上系统 ②编译版本(compileSdkVersion)API23以上
+ *
+ * 参考链接：http://blog.csdn.net/jianesrq0724/article/details/55211918
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 1;
+    private static final int MY_PERMISSIONS_REQUEST_CALL_CAMERA = 2;
+
+    // 声明一个数组，用来存储所有需要动态申请的权限
+    String[] permissions = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE,};
+    // 声明一个集合，在后面的代码中用来存储用户拒绝授权的权
+    List<String> mPermissionList = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+
+
+
+    }
+
+    private void initView() {
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+        findViewById(R.id.btn3).setOnClickListener(this);
+        findViewById(R.id.btn4).setOnClickListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_PERMISSIONS_REQUEST_CALL_PHONE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                showToast("权限已申请");
+            } else {
+                showToast("权限已拒绝");
+            }
+        }else if (requestCode == MY_PERMISSIONS_REQUEST_CALL_CAMERA){
+            for (int i = 0; i < grantResults.length; i++) {
+                if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                    //判断是否勾选禁止后不再询问
+                    boolean showRequestPermission = ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permissions[i]);
+                    if (showRequestPermission) {
+                        showToast("权限未申请");
+                    }
+                }
+            }
+        }
+    }
+
+    private void showToast(String string){
+        Toast.makeText(MainActivity.this,string,Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn1: //单个授权
+                //检查版本是否大于M
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                        ActivityCompat.requestPermissions(this,
+                                new String[]{Manifest.permission.CALL_PHONE},
+                                MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                    }else {
+                        showToast("权限已申请");
+                    }
+                }
+                break;
+            case R.id.btn2://多个授权
+                mPermissionList.clear();
+                for (int i = 0; i < permissions.length; i++) {
+                    if (ContextCompat.checkSelfPermission(MainActivity.this, permissions[i]) != PackageManager.PERMISSION_GRANTED) {
+                        mPermissionList.add(permissions[i]);
+                    }
+                }
+                if (mPermissionList.isEmpty()) {//未授予的权限为空，表示都授予了
+                    Toast.makeText(MainActivity.this,"已经授权",Toast.LENGTH_LONG).show();
+                } else {//请求权限方法
+                    String[] permissions = mPermissionList.toArray(new String[mPermissionList.size()]);//将List转为数组
+                    ActivityCompat.requestPermissions(MainActivity.this, permissions, MY_PERMISSIONS_REQUEST_CALL_CAMERA);
+                }
+                break;
+            case R.id.btn3:
+                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(camera, 1);
+                break;
+            case R.id.btn4:
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                Uri data = Uri.parse("tel:" + "10086");
+                intent.setData(data);
+                startActivity(intent);
+                break;
+        }
     }
 }

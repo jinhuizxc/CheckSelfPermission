6baf5ccaad460abdcb73ffabe79a31f2ae447e28
JinHui <hui.jin@boatchina.net>
2017-12-05 16:08:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/checkselfpermission/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.checkselfpermission;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Android 开发测试权限！项目中需要。
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
